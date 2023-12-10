efdbfacd379946207c245b54c456072566d1f86f
Emi1Mel0 <emilwagdy11@gmail.com>
2023-12-10 20:55:01 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,27 @@
+// Example
+
+// WeatherAPI.com API is so easy to implement. Look at following examples on how you can form a request to get data either through a web browser or in your application.
+
+//     So to get current weather for London: JSON: http://api.weatherapi.com/v1/current.json?key=570354fd2d8e4b739ae204611230903&q=London
+
+//     To get 7 day weather for US Zipcode 07112: JSON: http://api.weatherapi.com/v1/forecast.json?key=570354fd2d8e4b739ae204611230903&q=07112&days=7
+
+//     Search for cities starting with Lond: JSON: http://api.weatherapi.com/v1/search.json?key=570354fd2d8e4b739ae204611230903&q=lond
+
+//  570354fd2d8e4b739ae204611230903
+
+/*
+
+so for every thing i want to show
+there is api method for what i have its info
+and the parameter and the 
+
+*/
+
+var search = document.getElementById('search')
+
+function display() {
+    
+}
+
+// fetch(``);
